ebeffe603c89c8b45ed76b9e3c33f761fe03f502
Jim <jim1997jim@hotmail.com>
2020-03-06 14:19:19 +0200

    deleted useless

--- cart_config.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$host = 'localhost';
-$db = 'hci_app';
-$table = 'Cart';
-$username = 'hci_user';
-$password = 'DOoHd8EkdgTFjU2i';
-?>
--- login_config.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$host = 'localhost';
-$db = 'hci_app';
-$table = 'Users';
-$username = 'hci_user';
-$password = 'DOoHd8EkdgTFjU2i';
-?>
