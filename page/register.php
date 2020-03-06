938f251e7f1d441bd6d4a87ccdabc503b6866363
Jim <jim1997jim@hotmail.com>
2020-03-06 14:16:53 +0200

    moved files to page folder

---
+++ page/register.php
@@ -0,0 +1,22 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	if ((isset($_POST['name'])) && (isset($_POST['email'])) && (isset($_POST['password']))) {
+		include 'register_config.php';
+		$conn = new mysqli($host, $username, $password, $db);
+
+		if (!$conn->connect_error) {
+			$name = $conn->real_escape_string($_POST['name']);
+			$email = $conn->real_escape_string($_POST['email']);
+			$password = $conn->real_escape_string($_POST['password']);
+			
+			if ($conn->query("INSERT INTO $table (name, email, password) VALUES ('$name', '$email', '$password')") === TRUE) { 
+				echo json_encode('Success');
+			} else
+				echo json_encode(array('Error' => $conn->error));
+
+			$conn->close();
+		} else 
+			echo json_encode(array('Error' => $conn->connect_error));
+	}
+}
+?>
